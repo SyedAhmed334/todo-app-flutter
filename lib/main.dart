649d3d9f427abb56cf6e7b9e82c023e1bd39b416
Syed Ahmed Shah <ali.sain334@gmail.com>
2023-09-28 03:33:48 +0500

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({super.key});
+
+  @override
+  State<ToDoApp> createState() => _ToDoAppState();
+}
+
+class _ToDoAppState extends State<ToDoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
