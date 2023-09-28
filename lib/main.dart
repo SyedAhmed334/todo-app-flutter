6c609eeb7ceaf767a2d6ecf43d0d0db4ee10e31c
Syed Ahmed Shah <ali.sain334@gmail.com>
2023-09-29 03:02:09 +0500

    added model bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -35,80 +35,83 @@ class ToDoApp extends StatefulWidget {
 class _ToDoAppState extends State<ToDoApp> {
   Priority _selectedPriority = Priority.Normal;
   bool checkbox = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextField(
-                          decoration:
-                              InputDecoration(hintText: 'Add a new task'),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              'Task Priority:',
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.black),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            DropdownButton<Priority>(
-                              value: _selectedPriority,
-                              onChanged: (value) {
-                                setState(() {
-                                  _selectedPriority = value!;
-                                });
-                              },
-                              items: Priority.values.map((Priority priority) {
-                                return DropdownMenuItem<Priority>(
-                                  value: priority,
-                                  child:
-                                      Text(priority.toString().split('.').last),
-                                );
-                              }).toList(),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 100),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  Navigator.pop(context);
-                                });
-                              },
-                              child: Text('Save'),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
+              context: context,
+              builder: (context) => StatefulBuilder(
+                  builder: (context, StateSetter setModalState) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(hintText: 'Add a new task'),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            'Task Priority:',
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          DropdownButton<Priority>(
+                            value: _selectedPriority,
+                            onChanged: (value) {
+                              setModalState(() {
+                                if (value != null) {
+                                  _selectedPriority = value;
+                                }
+                              });
+                            },
+                            items: Priority.values.map((Priority priority) {
+                              return DropdownMenuItem<Priority>(
+                                value: priority,
+                                child:
+                                    Text(priority.toString().split('.').last),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 100),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
                                 Navigator.pop(context);
-                              },
-                              child: Text('Cancel'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                });
+                              });
+                            },
+                            child: Text('Save'),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Cancel'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            );
           },
           backgroundColor: Colors.green,
           foregroundColor: Colors.white,
